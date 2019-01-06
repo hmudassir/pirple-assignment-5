ff88d48432a71dc3e71d2c6eecc43a18dd4ba6f8
hmudassir <muhammad.mudassir@techtronix.bix>
2019-01-06 08:24:15 +0500

    added try catch

--- app/lib.js
+++ app/lib.js
@@ -1,11 +1,19 @@
 var lib  = {};
 
 lib._floor = function(_number){
-    return Math.floor(_number);
+    try{
+        return Math.floor(_number);
+    }catch(e){
+        return null;
+    }
 }
 
 lib._ceil = function(_number){
-    return Math.ceil(_number);
+    try{
+        return Math.ceil(_number);
+    }catch(e){
+        return null;
+    }
 }
 
 module.exports = lib;
